ecd5b0133a3f755193de29297f9bb576b91369b7
Matteo Crippa <matteo@boostco.de>
2018-01-23 22:29:50 +0100

    fix ccolor

--- src/components/Submit.vue
+++ src/components/Submit.vue
@@ -51,8 +51,8 @@
                                 <template slot-scope="{ save, cancel }">
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
-                                        <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
-                                        <v-btn flat color="primary" @click="save">OK</v-btn>
+                                        <v-btn flat color="deep-purple" @click="cancel">Cancel</v-btn>
+                                        <v-btn flat color="deep-purple" @click="save">OK</v-btn>
                                     </v-card-actions>
                                 </template>
                             </v-date-picker>
@@ -83,8 +83,8 @@
                                 <template slot-scope="{ save, cancel }">
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
-                                        <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
-                                        <v-btn flat color="primary" @click="save">OK</v-btn>
+                                        <v-btn flat color="deep-purple" @click="cancel">Cancel</v-btn>
+                                        <v-btn flat color="deep-purple" @click="save">OK</v-btn>
                                     </v-card-actions>
                                 </template>
                             </v-date-picker>
