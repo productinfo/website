0d2407dea380246346394857e09206c011d73351
Matteo Crippa <matteo@boostco.de>
2018-01-26 20:37:24 +0100

    fun copy for home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -82,7 +82,7 @@ export default {
   data () {
     return {
       welcomeMsg: '🔎 ',
-      welcomeMsg2: '+ conferences around the 🌍',
+      welcomeMsg2: '+ 🔥🔥 conferences around the 🌍',
       introMsg: 'Awesome Conference is a fully open sourced and collaborative project about conference listings, available for mobile too.',
       instructionMsg1: 'You can navigate conferences by category tapping just above ⬆️.',
       instructionMsg2: 'Or have a quick look at the last ⏰ conference added so far.',
