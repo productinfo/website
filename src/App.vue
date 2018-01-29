2b6126696750d9446890ed5d0371a0ed3025e81c
Matteo Crippa <matteo@boostco.de>
2018-01-29 18:33:40 +0100

    new logo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
         <v-content>
             <ul>
                 <li class="inline">
-                    <router-link :to="{ path: '/'}"><img src="./assets/logo-small.svg"></router-link>
+                    <router-link :to="{ path: '/'}"><img class="logo" src="./assets/logo-small.svg"></router-link>
                 </li>
                 <li class="inline" v-for="category in categories" v-bind:key="category.tag">
                     <router-link :to="`/category/${category.tag}`">
@@ -87,4 +87,7 @@ li.inline {
     margin: 0 10px;
     display: inline-flex;
 }
+    .logo {
+        width: 64px;
+    }
 </style>
