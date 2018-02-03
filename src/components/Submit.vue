c1f648e18feaea6aacaa8311c7801659395d2751
Matteo Crippa <matteo@boostco.de>
2018-02-03 13:51:47 +0100

    fix picker end start using start date

--- src/components/Submit.vue
+++ src/components/Submit.vue
@@ -1,198 +1,188 @@
 <template>
     <div class="submit">
-    <v-container grid-list-xl text-xs-left>
-        <v-layout row wrap>
-            <v-flex xs10 offset-xs1>
-                <v-card>
-                    <v-card-text>
-                    <v-form ref="form" lazy-validation v-model="valid">
-                        <p>Complete the form below to ask a new conference to be published:</p>
+        <v-container grid-list-xl text-xs-left>
+            <v-layout row wrap>
+                <v-flex xs10 offset-xs1>
+                    <v-card>
+                        <v-card-text>
+                            <v-form ref="form" lazy-validation v-model="valid">
+                                <p>Complete the form below to ask a new conference to be published:</p>
 
-                        <v-text-field
-                                label="Name"
-                                v-model="name"
-                                :counter="80"
-                                required
-                                :rules="nameRules"
-                                color="deep-purple"
-                        ></v-text-field>
+                                <v-text-field
+                                        label="Name"
+                                        v-model="name"
+                                        :counter="80"
+                                        required
+                                        :rules="nameRules"
+                                        color="deep-purple"
+                                ></v-text-field>
 
-                        <v-text-field
-                                label="Url"
-                                v-model="url"
-                                :counter="100"
-                                required
-                                :rules="urlRules"
-                                color="deep-purple"
-                        ></v-text-field>
+                                <v-text-field
+                                        label="Url"
+                                        v-model="url"
+                                        :counter="100"
+                                        required
+                                        :rules="urlRules"
+                                        color="deep-purple"
+                                ></v-text-field>
 
-                        <v-menu
-                                lazy
-                                :close-on-content-click="false"
-                                v-model="menustart"
-                                transition="scale-transition"
-                                offset-y
-                                full-width
-                                :nudge-right="40"
-                                max-width="290px"
-                                min-width="290px"
-                        >
-                            <v-text-field
-                                    slot="activator"
-                                    label="Start date"
-                                    v-model="startdate"
-                                    prepend-icon="event"
-                                    required
-                                    color="deep-purple"
-                                    :rules="dateRules"
-                                    readonly
-                            ></v-text-field>
-                            <v-date-picker v-model="startdate" no-title scrollable actions>
-                                <template slot-scope="{ save, cancel }">
-                                    <v-card-actions>
+                                <v-menu
+                                        ref="menustart"
+                                        lazy
+                                        :close-on-content-click="false"
+                                        v-model="menustart"
+                                        transition="scale-transition"
+                                        offset-y
+                                        full-width
+                                        :nudge-right="40"
+                                        min-width="330px"
+                                        :return-value.sync="startdate"
+                                >
+                                    <v-text-field
+                                            slot="activator"
+                                            label="Start date"
+                                            v-model="startdate"
+                                            prepend-icon="event"
+                                            readonly
+                                    ></v-text-field>
+                                    <v-date-picker v-model="startdate" no-title scrollable>
                                         <v-spacer></v-spacer>
-                                        <v-btn flat color="deep-purple" @click="cancel">Cancel</v-btn>
-                                        <v-btn flat color="deep-purple" @click="save">OK</v-btn>
-                                    </v-card-actions>
-                                </template>
-                            </v-date-picker>
-                        </v-menu>
+                                        <v-btn flat color="deep-purple" @click="menustart = false">Cancel</v-btn>
+                                        <v-btn flat color="deep-purple" @click="$refs.menustart.save(startdate)">OK
+                                        </v-btn>
+                                    </v-date-picker>
+                                </v-menu>
 
-                        <v-menu
-                                lazy
-                                :close-on-content-click="false"
-                                v-model="menuend"
-                                transition="scale-transition"
-                                offset-y
-                                full-width
-                                :nudge-right="40"
-                                max-width="290px"
-                                min-width="290px"
-                        >
-                            <v-text-field
-                                    slot="activator"
-                                    label="End date"
-                                    v-model="enddate"
-                                    prepend-icon="event"
-                                    required
-                                    color="deep-purple"
-                                    :rules="enddateRules"
-                                    readonly
-                            ></v-text-field>
-                            <v-date-picker v-model="enddate" no-title scrollable actions :picker-date="startdate">
-                                <template slot-scope="{ save, cancel }">
-                                    <v-card-actions>
+                                <v-menu
+                                        ref="menuend"
+                                        lazy
+                                        :close-on-content-click="false"
+                                        v-model="menuend"
+                                        transition="scale-transition"
+                                        offset-y
+                                        full-width
+                                        :nudge-right="40"
+                                        min-width="330px"
+                                        :return-value.sync="enddate"
+                                >
+                                    <v-text-field
+                                            slot="activator"
+                                            label="End date"
+                                            v-model="enddate"
+                                            prepend-icon="event"
+                                            readonly
+                                    ></v-text-field>
+                                    <v-date-picker v-model="enddate" no-title scrollable :picker-date="startdate"
+                                    >
                                         <v-spacer></v-spacer>
-                                        <v-btn flat color="deep-purple" @click="cancel">Cancel</v-btn>
-                                        <v-btn flat color="deep-purple" @click="save">OK</v-btn>
-                                    </v-card-actions>
-                                </template>
-                            </v-date-picker>
-                        </v-menu>
+                                        <v-btn flat color="deep-purple" @click="menuend = false">Cancel</v-btn>
+                                        <v-btn flat color="deep-purple" @click="$refs.menuend.save(enddate)">OK</v-btn>
+                                    </v-date-picker>
+                                </v-menu>
 
-                        <v-text-field
-                                label="Address"
-                                v-model="where"
-                                :counter="200"
-                                color="deep-purple"
-                        ></v-text-field>
+                                <v-text-field
+                                        label="Address"
+                                        v-model="where"
+                                        :counter="200"
+                                        color="deep-purple"
+                                ></v-text-field>
 
-                        <v-text-field
-                                label="City"
-                                v-model="city"
-                                :counter="100"
-                                required
-                                color="deep-purple"
-                                :rules="cityRules"
-                        ></v-text-field>
+                                <v-text-field
+                                        label="City"
+                                        v-model="city"
+                                        :counter="100"
+                                        required
+                                        color="deep-purple"
+                                        :rules="cityRules"
+                                ></v-text-field>
 
-                        <v-select
-                                color="deep-purple"
-                                label="Country"
-                                v-model="country"
-                                :items="countries"
-                                :rules="[v => !!v || 'Country is required']"
-                                required
-                                autocomplete
-                        ></v-select>
+                                <v-select
+                                        color="deep-purple"
+                                        label="Country"
+                                        v-model="country"
+                                        :items="countries"
+                                        :rules="[v => !!v || 'Country is required']"
+                                        required
+                                        autocomplete
+                                ></v-select>
 
-                        <v-checkbox
-                                label="Is Call for Paper open?"
-                                v-model="callforpaper"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="Is Call for Paper open?"
+                                        v-model="callforpaper"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-text-field
-                                label="Twitter handler"
-                                v-model="twitter"
-                                :counter="50"
-                                color="deep-purple"
-                        ></v-text-field>
+                                <v-text-field
+                                        label="Twitter handler"
+                                        v-model="twitter"
+                                        :counter="50"
+                                        color="deep-purple"
+                                ></v-text-field>
 
-                        <p>Categories</p>
-                        <v-checkbox
-                                label="Backend"
-                                v-model="backend"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <p>Categories</p>
+                                <v-checkbox
+                                        label="Backend"
+                                        v-model="backend"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-checkbox
-                                label="Frontend"
-                                v-model="frontend"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="Frontend"
+                                        v-model="frontend"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-checkbox
-                                label="DevOps"
-                                v-model="devops"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="DevOps"
+                                        v-model="devops"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-checkbox
-                                label="Marketing"
-                                v-model="marketing"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="Marketing"
+                                        v-model="marketing"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-checkbox
-                                label="Mobile"
-                                v-model="mobile"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="Mobile"
+                                        v-model="mobile"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-checkbox
-                                label="UI"
-                                v-model="ui"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="UI"
+                                        v-model="ui"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                        <v-checkbox
-                                label="UX"
-                                v-model="ux"
-                                color="deep-purple"
-                        ></v-checkbox>
+                                <v-checkbox
+                                        label="UX"
+                                        v-model="ux"
+                                        color="deep-purple"
+                                ></v-checkbox>
 
-                    </v-form>
-                    </v-card-text>
-                </v-card>
-                <br/>
+                            </v-form>
+                        </v-card-text>
+                    </v-card>
+                    <br/>
 
-                <v-btn @click="submit" color="deep-purple" dark>submit</v-btn>
+                    <v-btn @click="submit" color="deep-purple" dark>submit</v-btn>
 
-                <v-progress-circular indeterminate color="deep-purple" v-if="showSpinner"></v-progress-circular>
+                    <v-progress-circular indeterminate color="deep-purple" v-if="showSpinner"></v-progress-circular>
 
-                <v-alert v-if="submitSuccess" outline color="success" icon="check_circle" :value="true">
-                    Conference submitted.
-                </v-alert>
+                    <v-alert v-if="submitSuccess" outline color="success" icon="check_circle" :value="true">
+                        Conference submitted.
+                    </v-alert>
 
-                <v-alert v-if="submitFail" outline color="error" icon="warning" :value="true">
-                    There was an error, try again or contact using twitter <a href="https://twitter.com/aweconf">@aweconf</a>.
-                </v-alert>
+                    <v-alert v-if="submitFail" outline color="error" icon="warning" :value="true">
+                        There was an error, try again or contact using twitter <a href="https://twitter.com/aweconf">@aweconf</a>.
+                    </v-alert>
 
-            </v-flex>
+                </v-flex>
 
-        </v-layout>
-    </v-container>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
@@ -321,7 +311,7 @@ export default {
         category: categories
       }
 
-      axios.post('https://formspree.io/awc@boostco.de', { title: 'A new conference ' + this.name })
+      axios.post('https://formspree.io/awc@boostco.de', {title: 'A new conference ' + this.name})
         .then((resp) => {
           axios.post('https://aweconf.herokuapp.com/api/conference/submit', content)
             .then((resp) => {
