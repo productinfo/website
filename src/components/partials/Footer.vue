0cc0a6e3bae1eeee2878c3ba9c8d0efe981b4bd4
Matteo Crippa <matteo@boostco.de>
2018-02-05 11:21:11 +0100

    footer not fixed

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-footer app class="pa-3">
+    <v-footer class="pa-3">
         <v-spacer></v-spacer>
         <div>&copy; {{ year }} AweConf.com - <a href="https://twitter.com/aweconf" target="_blank">@aweconf</a> - <router-link :to="{ path: '/about' }" active-class="noneclass">About</router-link> - <router-link :to="`/submit`" active-class="noneclass">Submit</router-link> - <a href="https://www.iubenda.com/privacy-policy/7904617" target="_blank">Privacy</a> - <a href="https://www.iubenda.com/privacy-policy/7904617/cookie-policy" target="_blank">Cookie</a> Policy</div>
     </v-footer>
