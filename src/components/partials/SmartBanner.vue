9a6b8e4e4930f52cde1afcce3d82f354c4daeb3f
Matteo Crippa <matteo@boostco.de>
2018-02-06 16:12:38 +0100

    early implementation for smart banner

---
+++ src/components/partials/SmartBanner.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  props: {
+    'ios-url': String,
+    'android-url': String,
+    'app-name': String,
+    'app-price': {
+      type: String,
+      default: 'FREE'
+    }
+  },
+  data () {
+    return {
+      drawer: true,
+      mini: true,
+      right: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
