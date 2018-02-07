9b575606a8d4cbc2e7848f8fe1cb901e8082c484
Matteo Crippa <matteo@boostco.de>
2018-02-07 21:19:48 +0100

    improved category

--- src/components/Conference.vue
+++ src/components/Conference.vue
@@ -10,7 +10,7 @@
 
         <v-container fluid fill-height>
             <v-layout align-center justify-center class="text-xl-left text-md-left text-lg-left text-sm-left text-xs-left">
-                <v-flex xs12 sm12 md12>
+                <v-flex xs12 sm12 md10 xl10>
                     <v-card class="elevation-12">
                         <v-toolbar dark color="deep-purple">
                             <v-toolbar-title>{{ conference.title }} {{ conference.emojiflag }}</v-toolbar-title>
