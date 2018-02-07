48fffaff5734b8a6c536a19b346f55883ae750de
Matteo Crippa <matteo@boostco.de>
2018-02-07 20:40:23 +0100

    fix sorting

--- src/components/Unapproved.vue
+++ src/components/Unapproved.vue
@@ -102,8 +102,8 @@ export default {
         { text: 'Category', sortable: false, align: 'left' },
         { text: 'City', sortable: false, align: 'left', value: 'city' },
         { text: 'Country', sortable: false, align: 'left', value: 'country' },
-        { text: 'Start', value: 'startdate', align: 'left' },
-        { text: 'End', value: 'enddate', align: 'left' },
+        { text: 'Start', value: 'date.start', align: 'left' },
+        { text: 'End', value: 'date.end', align: 'left' },
         { text: 'Approve', sortable: false, align: 'left' }
       ]
     }
