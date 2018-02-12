57e0ef376c3ba05ec8423bc424df452c8c001db5
Matteo Crippa <matteo@boostco.de>
2018-02-12 16:08:40 +0100

    production

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import VueAnalytics from 'vue-analytics'
 import('../node_modules/vuetify/dist/vuetify.min.css')
+import 'babel-polyfill'
 // eslint-disable-next-line
 import { Store } from './store/store'
 
