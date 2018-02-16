222c402e4ee3f3fd4cbbebe3e8ff2db09ab721c6
Matteo Crippa <matteo@boostco.de>
2018-02-16 12:34:10 +0100

    fix

--- src/components/partials/Attendee.vue
+++ src/components/partials/Attendee.vue
@@ -2,10 +2,10 @@
     <v-card-text>
         <h2>Attendees</h2>
         <v-btn outline fab color="deep-purple" @click="triggerAttending" v-if="isAttendee">
-            <v-icon dark>add</v-icon>
+            <v-icon>add</v-icon>
         </v-btn>
         <v-btn fab color="deep-purple" @click="triggerAttending" v-else>
-            <v-icon dark>remove</v-icon>
+            <v-icon>remove</v-icon>
         </v-btn>
         <twitter-badge v-for="attendee in attendees" :account="attendee" :key="attendee"></twitter-badge>
     </v-card-text>
@@ -40,7 +40,7 @@ export default {
         })
     },
     checkUserIsAttendee () {
-      this.isAttendee = this.attendees.includes(this.$store.user.username)
+      this.isAttendee = !(this.attendees.indexOf(this.$store.user.username) !== -1)
     }
   }
 }
