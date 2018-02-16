5d820e14b905a5a4ae37971e6c24ce095e34ea65
Matteo Crippa <matteo@boostco.de>
2018-02-16 12:39:07 +0100

    fix

--- src/components/partials/Attendee.vue
+++ src/components/partials/Attendee.vue
@@ -1,12 +1,13 @@
 <template>
     <v-card-text>
         <h2>Attendees</h2>
-        <v-btn outline fab color="deep-purple" @click="triggerAttending" v-if="isAttendee">
-            <v-icon>add</v-icon>
-        </v-btn>
-        <v-btn fab color="deep-purple" @click="triggerAttending" v-else>
+        <v-btn fab color="deep-purple" @click="triggerAttending" v-if="isAttendee">
             <v-icon>remove</v-icon>
         </v-btn>
+        <v-btn outline fab color="deep-purple" @click="triggerAttending" v-else>
+            <v-icon>add</v-icon>
+        </v-btn>
+
         <twitter-badge v-for="attendee in attendees" :account="attendee" :key="attendee"></twitter-badge>
     </v-card-text>
 </template>
@@ -29,7 +30,7 @@ export default {
   },
   methods: {
     triggerAttending () {
-      if (this.$store.state.isAuthenticated == false) {
+      if (this.$store.state.isAuthenticated === false) {
         return alert('In order to add you as attendee, login first using twitter')
       }
 
