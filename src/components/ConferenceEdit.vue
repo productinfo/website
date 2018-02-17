c88117b53fe4339bcfb4e7977b954159f70e3c52
Matteo Crippa <matteo@boostco.de>
2018-02-17 14:57:41 +0100

    fix editing button

--- src/components/ConferenceEdit.vue
+++ src/components/ConferenceEdit.vue
@@ -3,7 +3,36 @@
 </template>
 
 <script>
+import axios from 'axios'
 
+export default {
+  name: 'ConferenceEdit',
+  data () {
+    return {
+    }
+  },
+
+  created () {
+    if (this.$session.get('role') === 'Admin') {
+      this.redirect()
+    } else {
+      // this.fetchData()
+    }
+  },
+
+  watch: {
+    '$route': 'fetchData'
+  },
+
+  methods: {
+    redirect () {
+      this.$router.push('/')
+    },
+    fetchData () {
+
+    }
+  }
+}
 </script>
 
 <style scoped>
