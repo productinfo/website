99a0610ab6de72a79d5e6205fcbaac83a2b4c4d2
Matteo Crippa <matteo@boostco.de>
2018-03-05 17:36:07 +0100

    fix ui home

--- src/components/partials/CategoriesList.vue
+++ src/components/partials/CategoriesList.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container grid-list-xl fluid fill-height>
         <v-layout row wrap>
-            <v-flex xs6 sm6 md1 xl1 v-for="category in categories" :key="category.tag" >
+            <v-flex xs6 sm6 md2 xl2 v-for="category in categories" :key="category.tag" >
                 <v-card class="elevation-1 pa-2 text-xs-center" :to="category.url">
                     <h1>{{ category.emoji }}</h1>
                     <h5>{{ category.title }}</h5>
