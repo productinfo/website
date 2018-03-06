a602b417391d66d5f31d6d23c112c54b0ef9629d
Matteo Crippa <matteo@boostco.de>
2018-03-06 11:52:07 +0100

    bigger images for cats

--- src/components/partials/CategoriesList.vue
+++ src/components/partials/CategoriesList.vue
@@ -4,7 +4,7 @@
             <v-flex xs6 sm6 md2 xl2 v-for="category in categories" :key="category.tag" >
                 <v-card class="elevation-1 pa-2 text-xs-center" :to="category.url">
                     <h1>{{ category.emoji }}</h1>
-                    <h5>{{ category.title }}</h5>
+                    <h3>{{ category.title }}</h3>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -21,4 +21,5 @@ export default {
 </script>
 
 <style scoped>
+    h1 { font-size: 80px; }
 </style>
